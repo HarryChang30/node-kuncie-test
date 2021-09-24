145c77d2c0aee1f8677e630253dbfbec82ddd5f3
harrychang <harrychang120@gmail.com>
2021-09-24 23:05:25 +0700

    initial project and requirement

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import dotenv  from 'dotenv';
+import app from '../src/app.js';
+
+dotenv.config();
+
+const port = process.env.NODE_PORT || 3000;
+
+const server = app.listen(port, () => console.log(`Server is running on the port: ${port}`));
+
+//Handling the graceful shutdown application
+const shutdown = () => {
+  if (!server.listening) process.exit(0);
+  server.close(err => {
+    if (err) {
+      console.error(err);
+      process.exit(1);
+    }
+    process.exit(0);
+  });
+};
+
+process.on('SIGINT', shutdown);
+process.on('SIGTERM', shutdown);
