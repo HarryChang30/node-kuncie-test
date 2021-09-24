2695a01955bb5f205537672bef1a95dbdbe70bf3
harrychang <harrychang120@gmail.com>
2021-09-24 23:49:50 +0700

    initialize model

---
+++ src/infrastructure/models/db.js
@@ -0,0 +1,35 @@
+import { Sequelize } from 'sequelize';
+
+const db = {};
+
+const sequelize = new Sequelize({
+  host: process.env.DB_HOST || '127.0.0.1',
+  port: process.env.DB_PORT || '3306',
+  dialect: 'mysql',
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000,
+  }
+});
+
+let models = [];
+
+//Initialize models
+models.forEach(model => {
+  const sequelizeModel = model(sequelize, Sequelize);
+  db[sequelizeModel.name] = sequelizeModel;
+});
+
+//Apply associations
+Object.keys(db).forEach(key => {
+  if ('associate' in db[key]) {
+    db[key].associate(db);
+  }
+});
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+module.exports = db;
